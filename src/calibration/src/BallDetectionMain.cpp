c3e425ec87b1d5b423e04e60053bd135cf89fa6a
Stefan Wrobel <stefan.w.ggb@gmail.com>
2013-05-06 13:44:26 +0200

    Add a simple unit test for BallDetection.

--- src/calibration/src/BallDetectionMain.cpp
+++ src/calibration/src/BallDetectionMain.cpp
@@ -8,6 +8,7 @@
 // ROS specific includes
 #include <ros/ros.h>
 #include <sensor_msgs/PointCloud2.h>
+#include <visualization_msgs/Marker.h>
 
 // PCL specific includes
 #include <pcl/ros/conversions.h>
@@ -29,6 +30,7 @@
 #include "../include/BallDetection.h"
 
 void msg_cb(const sensor_msgs::PointCloud2& input);
+void sendMarker(pcl::PointXYZ);
 
 int main(int argc, char **argv) {
 	ros::init(argc, argv, "BallDetectionMain");
@@ -44,17 +46,55 @@ void msg_cb(const sensor_msgs::PointCloud2& input) {
 	ROS_INFO("Pointcloud Message Received.");
 	BallDetection bc;
 
-	pcl::PointCloud<pcl::PointXYZRGB>::Ptr initialCloud = pcl::PointCloud<pcl::PointXYZRGB>::Ptr(new pcl::PointCloud<pcl::PointXYZRGB>());
+	pcl::PointCloud<pcl::PointXYZRGB>::Ptr initialCloud = pcl::PointCloud<
+			pcl::PointXYZRGB>::Ptr(new pcl::PointCloud<pcl::PointXYZRGB>());
 	pcl::fromROSMsg(input, *initialCloud);
-	bc.getPosition(initialCloud);
+	pcl::PointXYZ ballPosition = bc.getPosition(initialCloud);
 
-	pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloudWithoutPlanes = bc.getCloudWithoutPlanes();
-	pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloudWithBallOnly = bc.getCloudWithBallOnly();
+	pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloudWithoutPlanes =
+			bc.getCloudWithoutPlanes();
+	pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloudWithBallOnly =
+			bc.getCloudWithBallOnly();
+
+	//sendMarker(ballPosition);
+	ROS_INFO("Ball position is (x,y,z) (%f, %f, %f).", ballPosition.x, ballPosition.y, ballPosition.z);
 
 	pcl::visualization::CloudViewer viewer("viewer");
+
 	viewer.showCloud(cloudWithBallOnly);
 	while (!viewer.wasStopped()) {
 
 	}
 
+}
+
+void sendMarker(pcl::PointXYZ position) {
+	ros::NodeHandle n;
+	ros::Publisher marker_pub = n.advertise<visualization_msgs::Marker>(
+			"visualization_marker", 1);
+	visualization_msgs::Marker marker;
+	marker.header.frame_id = "torso";
+	marker.header.stamp = ros::Time();
+	marker.ns = "my_namespace";
+	marker.id = 0;
+	marker.type = visualization_msgs::Marker::SPHERE;
+	marker.action = visualization_msgs::Marker::ADD;
+	marker.pose.position.x = position.x;
+	marker.pose.position.y = position.y;
+	marker.pose.position.z = position.z;
+	marker.pose.orientation.x = 0.0;
+	marker.pose.orientation.y = 0.0;
+	marker.pose.orientation.z = 0.0;
+	marker.pose.orientation.w = 0.0;
+	marker.scale.x = 0.1;
+	marker.scale.y = 0.1;
+	marker.scale.z = 0.1;
+	marker.color.a = 1.0;
+	marker.color.r = 0.0;
+	marker.color.g = 1.0;
+	marker.color.b = 0.0;
+	while (ros::ok()) {
+		marker_pub.publish(marker);
+	}
+	ros::spinOnce();
 }
---
+++ src/calibration/test/BallDetectionTest.cpp
@@ -0,0 +1,68 @@
+//
+#include "../include/BallDetection.h"
+
+// pcl
+#include <pcl/point_cloud.h>
+#include <pcl/point_types.h>
+
+// gtest
+#include <gtest/gtest.h>
+
+// Declare a test
+/* Note: This is just a very simple test for verifying that the ball detection basically works.
+ * Passing this test does not imply that it also works with "real" data.
+ */
+TEST(BallDetectionTest, simpleTest)
+{
+	float x_center = 0.48;
+	float y_center = 0.24;
+	float z_center = 0.02;
+	int r = 1.10;
+
+	pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloudPtr = pcl::PointCloud<
+			pcl::PointXYZRGB>::Ptr(new pcl::PointCloud<pcl::PointXYZRGB>());
+	BallDetection bc;
+
+	// add sphere
+	for(int lat = 0; lat < 360; lat++) {
+		for(int lon = 0; lon < 180; lon++) {
+			float lon_rad = lon * M_PI / 180;
+			float lat_rad = lat * M_PI / 180;
+			pcl::PointXYZRGB point;
+			point.x = r*std::cos(lon_rad)*std::sin(lat_rad) + x_center;
+			point.y = r*std::sin(lon_rad)*std::sin(lat_rad) + y_center;
+			point.z = r*std::cos(lat_rad) + z_center;
+			point.rgb = 255 + (255 << 8) + (255 << 16);
+			(*cloudPtr).push_back(point);
+		}
+	}
+
+	// add plane
+	for(float x = 0; x < 1; x += 0.05) {
+		for(float y = 0;  y < 5; y += 0.05) {
+			pcl::PointXYZRGB point;
+			point.x = x;
+			point.y = y;
+			point.z = 0;
+			point.rgb = 255;
+			(*cloudPtr).push_back(point);
+		}
+	}
+
+	ASSERT_TRUE(std::abs(ballPosition.x - x_center) < 0.01);
+	ASSERT_TRUE(std::abs(ballPosition.y - y_center) < 0.01);
+	ASSERT_TRUE(std::abs(ballPosition.z - z_center) < 0.01);
+}
+
+// Run all the tests that were declared with TEST()
+int main(int argc, char **argv){
+testing::InitGoogleTest(&argc, argv);
+return RUN_ALL_TESTS();
+}
+
+
+
+
+
+
+
