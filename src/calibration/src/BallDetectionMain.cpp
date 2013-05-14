cf7c9d9a9ef4a2ca373bba03fece2418cc735ae0
Stefan Wrobel <stefan.w.ggb@gmail.com>
2013-05-14 14:25:07 +0200

    Show marker.

--- src/calibration/src/BallDetectionMain.cpp
+++ src/calibration/src/BallDetectionMain.cpp
@@ -19,7 +19,7 @@
 #include "../include/BallDetection.h"
 
 void msg_cb(const sensor_msgs::PointCloud2& input);
-void sendMarker(pcl::PointXYZ);
+void sendMarker(pcl::PointXYZ, ros::Time stamp);
 
 int main(int argc, char **argv) {
 	ros::init(argc, argv, "BallDetectionMain");
@@ -45,25 +45,26 @@ void msg_cb(const sensor_msgs::PointCloud2& input) {
 	pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloudWithBallOnly =
 			bc.getCloudWithBallOnly();
 
-	//sendMarker(ballPosition);
+	ros::Time stamp = input.header.stamp;
+	sendMarker(ballPosition, stamp);
 	ROS_INFO("Ball position is (x,y,z) (%f, %f, %f).", ballPosition.x, ballPosition.y, ballPosition.z);
 
 	pcl::visualization::CloudViewer viewer("viewer");
 
-	viewer.showCloud(cloudWithBallOnly);
+	/*viewer.showCloud(cloudWithBallOnly);
 	while (!viewer.wasStopped()) {
 
-	}
+	}*/
 
 }
 
-void sendMarker(pcl::PointXYZ position) {
+void sendMarker(pcl::PointXYZ position, ros::Time stamp) {
 	ros::NodeHandle n;
 	ros::Publisher marker_pub = n.advertise<visualization_msgs::Marker>(
 			"visualization_marker", 1);
 	visualization_msgs::Marker marker;
-	marker.header.frame_id = "torso";
-	marker.header.stamp = ros::Time();
+	marker.header.frame_id = "xtion_depth_optical_frame";
+	marker.header.stamp = stamp;
 	marker.ns = "my_namespace";
 	marker.id = 0;
 	marker.type = visualization_msgs::Marker::SPHERE;
@@ -75,9 +76,9 @@ void sendMarker(pcl::PointXYZ position) {
 	marker.pose.orientation.y = 0.0;
 	marker.pose.orientation.z = 0.0;
 	marker.pose.orientation.w = 0.0;
-	marker.scale.x = 0.1;
-	marker.scale.y = 0.1;
-	marker.scale.z = 0.1;
+	marker.scale.x = 0.15;
+	marker.scale.y = 0.15;
+	marker.scale.z = 0.15;
 	marker.color.a = 1.0;
 	marker.color.r = 0.0;
 	marker.color.g = 1.0;
