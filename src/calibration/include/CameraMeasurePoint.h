a07e79f5d44a321e1fdb78c59647336f474cb04b
Stefan Wrobel <wrobels@informatik.uni-freiburg.de>
2013-07-01 14:58:57 +0200

    Added new transform to measurement.

--- src/calibration/include/CameraMeasurePoint.h
+++ src/calibration/include/CameraMeasurePoint.h
@@ -18,7 +18,10 @@ public:
 	tf::Vector3 measuredPosition; // measured point within the optical frame
 	tf::Transform opticalToCamera; // e.g. measure frame to first frame of the camera system
 	tf::Transform headToFixed; // e.g. first frame of body (HeadPitch) to last frame of body (r_sole)
+	tf::Transform fixedToFootprint;
 	ros::Time stamp;
+
+	inline tf::Transform headToFootprint(){return headToFixed * fixedToFootprint;}
 };
 
 typedef CameraMeasurePoint MeasurePoint;
