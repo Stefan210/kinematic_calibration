d9ec0258341d3d0a5f5ba3bc3c8c9e0ea9c49974
Stefan Wrobel <wrobels@informatik.uni-freiburg.de>
2013-07-03 14:58:11 +0200

    Implemented stream operators for CameraMeasurePoint.

--- src/calibration/include/CameraMeasurePoint.h
+++ src/calibration/include/CameraMeasurePoint.h
@@ -10,6 +10,7 @@
 
 #include <tf/tf.h>
 #include "../include/GroundDetection.h"
+#include "../include/Utils.h"
 
 class CameraMeasurePoint {
 public:
@@ -25,16 +26,28 @@ public:
 
 	inline const tf::Transform headToFootprint() const {return headToFixed * fixedToFootprint;}
 	inline const tf::Pose groundPose() const {return groundData.getPose();}
-/*
-	friend ostream &operator<<(ostream &output, const CameraMeasurePoint &gd) {
-		output << " " << gd.a << " " << gd.b << " " << gd.c << " " << gd.d;
+
+	friend ostream &operator<<(ostream &output, const CameraMeasurePoint &cmp) {
+		output << " " << cmp.measuredPosition;
+		output << " " << cmp.groundData;
+		output << " " << cmp.opticalToCamera;
+		output << " " << cmp.headToFixed;
+		output << " " << cmp.fixedToFootprint;
+		output << " " << cmp.stamp.sec;
+		output << " " << cmp.stamp.nsec;
 		return output;
 	}
 
-	friend istream &operator>>(istream &input, CameraMeasurePoint &gd) {
-		input >> gd.a >> gd.b >> gd.c >> gd.d;
+	friend istream &operator>>(istream &input, CameraMeasurePoint &cmp) {
+		input >> cmp.measuredPosition;
+		input >> cmp.groundData;
+		input >> cmp.opticalToCamera;
+		input >> cmp.headToFixed;
+		input >> cmp.fixedToFootprint;
+		input >> cmp.stamp.sec;
+		input >> cmp.stamp.nsec;
 		return input;
-	}*/
+	}
 };
 
 typedef CameraMeasurePoint MeasurePoint;
--- src/calibration/include/Utils.h
+++ src/calibration/include/Utils.h
@@ -10,10 +10,17 @@
 
 #include <iostream>
 #include <tf/tf.h>
+#include <ros/ros.h>
 
 using namespace std;
 
 ostream &operator<<(ostream &output, const tf::Vector3 &v);
 istream &operator>>(istream &input, tf::Vector3 &v);
+
+ostream &operator<<(ostream &output, const tf::Quaternion &q);
+istream &operator>>(istream &input, tf::Quaternion &q);
+
+ostream &operator<<(ostream &output, const tf::Transform &t);
+istream &operator>>(istream &input, tf::Transform &t);
 
 #endif /* UTILS_H_ */
