2c1b616b14e669d4f1a3a04872493dc1816ea063
Stefan Wrobel <wrobels@informatik.uni-freiburg.de>
2013-07-10 10:02:20 +0200

    Added new unit test for ground detection.

--- src/calibration/test/GroundDetectionTest.cpp
+++ src/calibration/test/GroundDetectionTest.cpp
@@ -10,6 +10,7 @@
 // PCL specific includes
 #include <pcl/point_cloud.h>
 #include <pcl/point_types.h>
+#include <pcl/common/transforms.h>
 
 // gtest specific includes
 #include <gtest/gtest.h>
@@ -39,6 +40,73 @@ TEST(GroundDetectionTest, simpleTest) {
 	ASSERT_DOUBLE_EQ(0.0, groundData.b);
 	ASSERT_DOUBLE_EQ(1.0, groundData.c);
 	ASSERT_DOUBLE_EQ(0.0, groundData.d);
+}
+
+TEST(GroundDetectionTest, transformTest) {
+
+	pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloudPtr = pcl::PointCloud<
+			pcl::PointXYZRGB>::Ptr(new pcl::PointCloud<pcl::PointXYZRGB>());
+	GroundDetection gd;
+
+	// add plane
+	for (float x = 0; x < 1; x += 0.005) {
+		for (float y = 0; y < 3; y += 0.005) {
+			pcl::PointXYZRGB point;
+			point.x = x;
+			point.y = y;
+			point.z = 0;
+			point.rgb = 255;
+			(*cloudPtr).push_back(point);
+		}
+	}
+
+	// test basic functionality
+	GroundData groundData = gd.getGroundData(cloudPtr);
+	double r, p, y;
+	groundData.getRPY(r, p, y);
+	ASSERT_NEAR(0, r, 1e-6);
+	ASSERT_NEAR(0, p, 1e-6);
+
+	// create Transformation
+	tf::Quaternion rot;
+	rot.setRPY(M_PI_2, M_PI_4, M_PI_4);
+	tf::Vector3 trans(0.001, 0.002, 0.003);
+	tf::Transform transform(rot, trans);
+
+	Eigen::Quaternionf rot_eigen(rot.w(), rot.x(), rot.y(), rot.z());
+	Eigen::Vector3f trans_eigen(1, 2, 3);
+
+	// transform pointcloud
+	pcl::PointCloud<pcl::PointXYZRGB>::Ptr transformedCloudPtr = pcl::PointCloud<
+			pcl::PointXYZRGB>::Ptr(new pcl::PointCloud<pcl::PointXYZRGB>());
+
+	pcl::transformPointCloud(*cloudPtr, *transformedCloudPtr, trans_eigen, rot_eigen);
+
+	// test transformed cloud
+	ASSERT_EQ((*cloudPtr).width, (*transformedCloudPtr).width);
+	groundData = gd.getGroundData(transformedCloudPtr);
+	groundData.getRPY(r, p, y);
+	ASSERT_NEAR(M_PI_2, r, 1e-3);
+	ASSERT_NEAR(M_PI_2, p, 1e-3);
+
+	// transform normal vector
+	tf::Vector3 normalVector(groundData.a, groundData.b, groundData.c);
+	tf::Vector3 transformedNormalVector = transform.getBasis().transpose() * normalVector;
+	groundData.a = transformedNormalVector[0];
+	groundData.b = transformedNormalVector[1];
+	groundData.c = transformedNormalVector[2];
+	groundData.getRPY(r, p, y);
+	ASSERT_NEAR(0, r, 1e-3);
+	ASSERT_NEAR(0, p, 1e-3);
+
+	/*
+	// transform pose
+	groundData = gd.getGroundData(transformedCloudPtr);
+	tf::Pose transformedPose = transform.inverse() * groundData.getPose();
+	transformedPose.getBasis().getRPY(r, p, y);
+	ASSERT_NEAR(0, r, 1e-3);
+	ASSERT_NEAR(0, p, 1e-3);
+	*/
 }
 
 TEST(GroundDataTest, getRpyTest1) {
