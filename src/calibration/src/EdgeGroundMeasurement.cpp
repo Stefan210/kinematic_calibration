dc9f95e8848bc9a6e929bef819bba474ac29a468
Stefan Wrobel <wrobels@informatik.uni-freiburg.de>
2013-07-11 17:01:26 +0200

    Refactoring: Removed unused code.

--- src/calibration/src/EdgeGroundMeasurement.cpp
+++ src/calibration/src/EdgeGroundMeasurement.cpp
@@ -25,28 +25,9 @@ void EdgeGroundMeasurement::computeError() {
 	// calculate error from ground in respect to roll and pitch (which should be 0)
 	double roll, pitch, yaw;
 
-	/*
-	tf::Pose transformedGroundPose =
-			(measurePoint.headToFootprint()
-					* (cameraToHeadTransform
-							* (measurePoint.opticalToCamera
-									* measurePoint.groundPose())));
-
-	tf::Matrix3x3(transformedGroundPose.getRotation()).getRPY(roll, pitch, yaw);
-	roll = GroundData::normalize(roll);
-	pitch = GroundData::normalize(pitch);
-	yaw = GroundData::normalize(yaw);
-	std::cout << "ground (1: via pose transformation)(roll, pitch, yaw) "
-			<< roll << " " << pitch << " " << yaw << std::endl;
-	*/
-
 //	double roll, pitch, yaw;
 	tf::Vector3 groundNormal(measurePoint.groundData.a,
 			measurePoint.groundData.b, measurePoint.groundData.c);
-//	tf::Vector3 transformedGroundNormal = measurePoint.fixedToFootprint.getBasis()
-//			* (measurePoint.headToFixed.getBasis()
-//					* (cameraToHeadTransform.getBasis()
-//							* (measurePoint.opticalToCamera.getBasis() * groundNormal)));
 
 	tf::Vector3 transformedGroundNormal = (measurePoint.opticalToFootprint(
 			cameraToHeadTransform).getBasis().inverse()).transpose()
@@ -58,7 +39,7 @@ void EdgeGroundMeasurement::computeError() {
 	transformedGroundData.c = transformedGroundNormal[2];
 	transformedGroundData.d = 0;
 	transformedGroundData.getRPY(roll, pitch, yaw);
-//	std::cout << "ground (2: via normal transformation)(roll, pitch, yaw) "
+//	std::cout << "ground (roll, pitch, yaw) "
 //			<< roll << " " << pitch << " " << yaw << std::endl;
 
 	_error[0] = roll;
