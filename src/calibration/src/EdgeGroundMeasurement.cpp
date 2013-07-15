d87e4b613c9b7326b968770693334bbd88770163
Stefan Wrobel <wrobels@informatik.uni-freiburg.de>
2013-07-15 13:34:34 +0200

    Bugfix: Calculated the "wrong" transformation.

--- src/calibration/src/EdgeGroundMeasurement.cpp
+++ src/calibration/src/EdgeGroundMeasurement.cpp
@@ -25,24 +25,27 @@ void EdgeGroundMeasurement::computeError() {
 	// calculate error from ground in respect to roll and pitch (which should be 0)
 	double roll, pitch, yaw;
 
-//	double roll, pitch, yaw;
-	tf::Vector3 groundNormal(measurePoint.groundData.a,
-			measurePoint.groundData.b, measurePoint.groundData.c);
+	tf::Transform transform;
+	transform = measurePoint.opticalToFootprint(cameraToHeadTransform);
+	GroundData transformedGroundData = measurePoint.groundData.transform(transform);
 
-	tf::Vector3 transformedGroundNormal = (measurePoint.opticalToFootprint(
-			cameraToHeadTransform).getBasis().inverse()).transpose()
-			* groundNormal;
-
-	GroundData transformedGroundData;
-	transformedGroundData.a = transformedGroundNormal[0];
-	transformedGroundData.b = transformedGroundNormal[1];
-	transformedGroundData.c = transformedGroundNormal[2];
-	transformedGroundData.d = 0;
+	double a = transformedGroundData.a / transformedGroundData.c;
+	double b = transformedGroundData.b / transformedGroundData.c;
+	double c = transformedGroundData.c / transformedGroundData.c;
+	double d = transformedGroundData.d / transformedGroundData.c;
 	transformedGroundData.getRPY(roll, pitch, yaw);
+
 //	std::cout << "ground (roll, pitch, yaw) "
-//			<< roll << " " << pitch << " " << yaw << std::endl;
+//				<< roll << " " << pitch << " " << yaw << " ";
+//	std::cout << a << "x+" << b << "y+" << c << "z+" << d << "=0" << std::endl;
 
 	_error[0] = roll;
 	_error[1] = pitch;
+
+	_error[0] = fabs(d);
+	_error[1] = tf::Vector3(a, b, c).normalized().angle(tf::Vector3(0, 0, 1));
+
+//	_error[0] = fabs(roll) < 0.05  ? 0 : 1000;
+//	_error[0] = fabs(roll) < 0.05  ? 0 : 1000;
 }
 
