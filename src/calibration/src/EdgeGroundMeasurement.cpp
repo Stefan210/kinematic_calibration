f3bd7862af1f55b9cdd6fd643261686236ad86df
Stefan Wrobel <wrobels@informatik.uni-freiburg.de>
2013-07-29 17:34:20 +0200

    Bugfix: 0.20 -> 0.02 (see last commit)

--- src/calibration/src/EdgeGroundMeasurement.cpp
+++ src/calibration/src/EdgeGroundMeasurement.cpp
@@ -35,13 +35,16 @@ void EdgeGroundMeasurement::computeError() {
 			measurePoint.withHeadPitchOffset(headPitchOffset).withHeadYawOffset(
 					headYawOffset).groundData.transform(transform);
 
-	double length = transformedGroundData.a + transformedGroundData.b
-			+ transformedGroundData.c + transformedGroundData.d;
+	transformedGroundData.getRPY(roll, pitch, yaw);
+
+	double length = fabs(transformedGroundData.a) + fabs(transformedGroundData.b)
+			+ fabs(transformedGroundData.c) + fabs(transformedGroundData.d);
+	length *= transformedGroundData.c > 0 ? 1 : -1;
 	double a = transformedGroundData.a / length;
 	double b = transformedGroundData.b / length;
 	double c = transformedGroundData.c / length;
 	double d = transformedGroundData.d / length;
-	transformedGroundData.getRPY(roll, pitch, yaw);
+
 
 //	std::cout << "ground (roll, pitch, yaw) "
 //				<< roll << " " << pitch << " " << yaw << " ";
@@ -50,7 +53,7 @@ void EdgeGroundMeasurement::computeError() {
 //	_error[0] = roll;
 //	_error[1] = pitch;
 
-	_error[0] = fabs(d) - 0.2;
+	_error[0] = fabs(d) - 0.02;
 	_error[1] = tf::Vector3(a, b, c).normalized().angle(tf::Vector3(0, 0, 1));
 
 //	_error[0] = fabs(roll) < 0.05  ? 0 : 1000;
