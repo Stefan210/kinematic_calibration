9c0ee1526f06eb3952ae977671ce9c9d5f1d99d1
Stefan Wrobel <wrobels@informatik.uni-freiburg.de>
2013-09-03 16:13:04 +0200

    Added doxygen comments to CalibrationDataSerialization.

--- src/calibration/include/CalibrationDataSerialization.h
+++ src/calibration/include/CalibrationDataSerialization.h
@@ -19,29 +19,77 @@
 
 using namespace std;
 
-/*
- *
+/**
+ * Class for serializing and deserializing data used for transformation.
  */
 class CalibrationDataSerialization: public CameraTransformOptimization {
 public:
+	/**
+	 * Constructor.
+	 * @param[in] dataCaptureParameter Parameter used for data capture.
+	 * @param[in] filename Name of the file for writing the serialized data.
+	 */
 	CalibrationDataSerialization(DataCaptureParameter dataCaptureParameter,
 			std::string filename);
+
+	/**
+	 * Deconstructor.
+	 */
 	virtual ~CalibrationDataSerialization();
 
+	/**
+	 * Serializes the data.
+	 * @param[out] calibrationState Does nothing.
+	 */
 	virtual void optimizeTransform(CalibrationState& calibrationState);
 
+	/**
+	 * Deserializes the data and returns the measure points.
+	 * @return Deserialized measure points.
+	 */
 	virtual std::vector<MeasurePoint> getMeasurementSeries();
+
+	/**
+	 * Deserializes the data and returns the initial transform.
+	 * @return Deserialized initial transform.
+	 */
 	virtual tf::Transform getInitialTransform();
 
 protected:
+	/**
+	 * Writes the serialized data to file.
+	 */
 	void saveToFile();
+
+	/**
+	 * Loads the serialized data from file.
+	 */
 	void loadFromFile();
 
 private:
+	/**
+	 * Filename where the data should be saved.
+	 */
 	std::string fileName;
+
+	/**
+	 * Indicates whether the data was already loaded.
+	 */
 	bool dataLoaded;
+
+	/**
+	 * Indicates whether the data was already saved.
+	 */
 	bool dataSaved;
+
+	/**
+	 * Holds the initial transform.
+	 */
 	tf::Transform initialTransformCameraToHead;
+
+	/**
+	 * Holds the dataCaptureParameter.
+	 */
 	DataCaptureParameter dataCaptureParameter;
 };
 
