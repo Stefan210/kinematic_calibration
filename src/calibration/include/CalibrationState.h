4da04445ab399bce97269b95b5ad084622834a6f
Stefan Wrobel <wrobels@informatik.uni-freiburg.de>
2013-09-03 21:18:02 +0200

    Added doxygen comments to CalibrationState.

--- src/calibration/include/CalibrationState.h
+++ src/calibration/include/CalibrationState.h
@@ -10,25 +10,80 @@
 
 #include <tf/tf.h>
 
-/*
- *
+/**
+ * Class representing a calibration state.
  */
 class CalibrationState {
 public:
+	/**
+	 * Default constructor. Initializes all members with zero-values.
+	 */
 	CalibrationState();
+
+	/**
+	 * Parameterized constructor.
+	 * @param cameraToHead Transformation to be optimized.
+	 * @param headYawOffset Current headYaw joint offset.
+	 * @param headPitchOffset Current headPitch joint offset.
+	 */
 	CalibrationState(tf::Transform cameraToHead, double headYawOffset,
 			double headPitchOffset);
+
+	/**
+	 * Deconstructor.
+	 */
 	virtual ~CalibrationState();
+
+	/**
+	 * Returns the cameraToHead transform.
+	 * @return The cameraToHead transform.
+	 */
 	tf::Transform getCameraToHead() const;
+
+	/**
+	 * Sets the cameraToHead transform.
+	 * @param cameraToHead Transform to be set.
+	 */
 	void setCameraToHead(const tf::Transform& cameraToHead);
+
+	/**
+	 * Returns the current headPitch joint offset.
+	 * @return The current headPitch joint offset.
+	 */
 	double getHeadPitchOffset() const;
+
+	/**
+	 * Returns the current headPitch joint offset.
+	 * @return The current headPitch joint offset.
+	 */
 	void setHeadPitchOffset(double headPitchOffset);
+
+	/**
+	 * Returns the current headYaw joint offset.
+	 * @return The current headYaw joint offset.
+	 */
 	double getHeadYawOffset() const;
+
+	/**
+	 * Returns the current headYaw joint offset.
+	 * @return The current headYaw joint offset.
+	 */
 	void setHeadYawOffset(double headYawOffset);
 
 protected:
+	/**
+	 * Transformation to be optimized.
+	 */
 	tf::Transform cameraToHead;
+
+	/**
+	 * headYawOffset Current headYaw joint offset.
+	 */
 	double headYawOffset;
+
+	/**
+	 * headPitchOffset Current headPitch joint offset.
+	 */
 	double headPitchOffset;
 };
 
