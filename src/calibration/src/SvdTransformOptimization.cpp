21b82e4715b906dc84a192d4607374bc50d77440
Stefan Wrobel <wrobels@informatik.uni-freiburg.de>
2013-09-30 21:35:22 +0200

    Minor.

--- src/calibration/src/SvdTransformOptimization.cpp
+++ src/calibration/src/SvdTransformOptimization.cpp
@@ -13,9 +13,9 @@
 
 SvdTransformOptimization::SvdTransformOptimization(
 		CameraTransformOptimizationParameter param) :
-		CameraTransformOptimization(param), maxIterations(100000), minError(
+		CameraTransformOptimization(param), minError(
 				0.000001), errorImprovement(0.000000001) {
-	// TODO Auto-generated constructor stub
+	this->setMaxIterations(100000);
 
 }
 
