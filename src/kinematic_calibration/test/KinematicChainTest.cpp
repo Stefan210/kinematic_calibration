6bacf168d037807fac1a2cee7cb35e228f0519ef
Stefan Wrobel <wrobels@informatik.uni-freiburg.de>
2013-10-27 15:32:20 +0100

    Add a simple unit test for KinematicChain.
    (Loads a robot model using ModelLoader, extracts a chain and tests the
    number of segments.)

--- src/kinematic_calibration/test/KinematicChainTest.cpp
+++ src/kinematic_calibration/test/KinematicChainTest.cpp
@@ -6,7 +6,34 @@
  */
 
 #include "../include/KinematicChain.h"
+#include "../include/ModelLoader.h"
+
+#include <gtest/gtest.h>
+#include <fstream>
 
 namespace kinematic_calibration {
 
+TEST(KinematicChainTest, modelLoaderIntegrationTest) {
+	// arrange
+	ifstream file("test.urdf");
+	std::string urdfStr((std::istreambuf_iterator<char>(file)),
+	                 std::istreambuf_iterator<char>());
+	ModelLoader modelLoader;
+	modelLoader.initializeFromUrdf(urdfStr);
+	KDL::Tree tree;
+	modelLoader.getKdlTree(tree);
+
+	// act
+	KinematicChain kinematicChain(tree, "base_link", "HeadYaw_link");
+
+	// assert
+	ASSERT_EQ(2, static_cast<int>(kinematicChain.getChain().segments.size()));
+}
+
 } /* namespace kinematic_calibration */
+
+// Run all the tests that were declared with TEST()
+int main(int argc, char **argv) {
+	testing::InitGoogleTest(&argc, argv);
+	return RUN_ALL_TESTS();
+}
