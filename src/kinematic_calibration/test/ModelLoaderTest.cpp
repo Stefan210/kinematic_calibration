f7484b25d599a6efd5814c93e3d7e42f3742dcfe
Stefan Wrobel <wrobels@informatik.uni-freiburg.de>
2013-10-27 13:58:44 +0100

    Implemented a simple unit test for ModelLoader that checks whether a model from a xml/urdf string can be loaded.

--- src/kinematic_calibration/test/ModelLoaderTest.cpp
+++ src/kinematic_calibration/test/ModelLoaderTest.cpp
@@ -5,8 +5,36 @@
  *      Author: stefan
  */
 
-#include "/home/stefan/catkin_ws/src/kinematic_calibration/include/ModelLoader.h"
+#include "../include/ModelLoader.h"
+
+#include <gtest/gtest.h>
+#include <fstream>
+#include <string>
+#include <sstream>
+
+using namespace std;
 
 namespace kinematic_calibration {
 
+TEST(ModelLoaderTest, initializeFromUrdfTest) {
+	// arrange
+	ifstream file("test.urdf");
+	std::string urdfStr((std::istreambuf_iterator<char>(file)),
+	                 std::istreambuf_iterator<char>());
+	ModelLoader modelLoader;
+
+	// act
+	bool success;
+	success = modelLoader.initializeFromUrdf(urdfStr);
+
+	// assert
+	ASSERT_TRUE(success);
+}
+
 } /* namespace kinematic_calibration */
+
+// Run all the tests that were declared with TEST()
+int main(int argc, char **argv) {
+	testing::InitGoogleTest(&argc, argv);
+	return RUN_ALL_TESTS();
+}
