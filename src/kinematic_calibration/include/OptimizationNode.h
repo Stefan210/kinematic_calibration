94fef6cd79c458a949c09d1709cb528dfb905cff
Stefan Wrobel <wrobels@informatik.uni-freiburg.de>
2013-11-06 19:26:16 +0100

    Add stub for the optimization node.

---
+++ src/kinematic_calibration/include/OptimizationNode.h
@@ -0,0 +1,23 @@
+/*
+ * OptimizationNode.h
+ *
+ *  Created on: 06.11.2013
+ *      Author: stefan
+ */
+
+#ifndef OPTIMIZATIONNODE_H_
+#define OPTIMIZATIONNODE_H_
+
+namespace kinematic_calibration {
+
+/*
+ *
+ */
+class OptimizationNode {
+public:
+	OptimizationNode();
+	virtual ~OptimizationNode();
+};
+
+} /* namespace kinematic_calibration */
+#endif /* OPTIMIZATIONNODE_H_ */
---
+++ src/kinematic_calibration/src/OptimizationNode.cpp
@@ -0,0 +1,25 @@
+/*
+ * OptimizationNode.cpp
+ *
+ *  Created on: 06.11.2013
+ *      Author: stefan
+ */
+
+#include "../include/OptimizationNode.h"
+
+namespace kinematic_calibration {
+
+OptimizationNode::OptimizationNode() {
+	// TODO Auto-generated constructor stub
+
+}
+
+OptimizationNode::~OptimizationNode() {
+	// TODO Auto-generated destructor stub
+}
+
+} /* namespace kinematic_calibration */
+
+int main(void) {
+	return 0;
+}
