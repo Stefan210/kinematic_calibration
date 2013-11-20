1bf6a65680728c59278519bc2f4c81f212d68729
Stefan Wrobel <wrobels@informatik.uni-freiburg.de>
2013-11-20 16:37:54 +0100

    Add a script for re-indexing poses with a specific prefix. E.g. larm001a..., larm001b..., larm003..., ... becomes larm001..., larm002..., larm003..., ...

---
+++ src/kinematic_calibration/scripts/PoseReindexer.py
@@ -0,0 +1,39 @@
+import sys
+import yaml
+from sympy.printing.str import sstr
+
+
+class PoseReindexer():
+    
+    def __init__(self):
+        self.poses = dict()
+        
+    def loadFromYamlFile(self, in_file):
+        f = open(in_file, "r")
+        for k, v in yaml.load(f).iteritems():
+            self.poses[k] = v
+        f.close()
+        
+    def saveToYamlFile(self, out_file):
+        f = open(out_file, "w")
+        yaml.dump(self.poses, f)        
+        f.close()
+        
+    def reindex(self, prefix):
+        i = 1
+        new_poses = dict()
+        keys_sorted = sorted(self.poses.keys())
+        for key in keys_sorted:
+            if key[:len(prefix)] == prefix:
+                new_poses[prefix + str('%03d' % i)] = self.poses[key]
+                i = i + 1
+        self.poses = new_poses
+
+if __name__ == '__main__':
+    if len(sys.argv) != 4:
+        print "Arguments: <in_file> <out_file> <prefix>"
+    poseReindexer = PoseReindexer()
+    poseReindexer.loadFromYamlFile(in_file=sys.argv[1])
+    poseReindexer.reindex(prefix=sys.argv[3])
+    poseReindexer.saveToYamlFile(out_file=sys.argv[2])
+    exit(0)
