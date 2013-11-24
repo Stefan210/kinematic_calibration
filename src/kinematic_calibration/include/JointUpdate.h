d6a676304f99366991f0f628b07adc71f41c289b
Stefan Wrobel <wrobels@informatik.uni-freiburg.de>
2013-11-24 22:47:17 +0100

    Add missing header.

---
+++ src/kinematic_calibration/include/JointUpdate.h
@@ -0,0 +1,47 @@
+/*
+ * JointUpdate.h
+ *
+ *  Created on: 22.11.2013
+ *      Author: stefan
+ */
+
+#ifndef JOINTUPDATE_H_
+#define JOINTUPDATE_H_
+
+#include <urdf/model.h>
+#include <urdf_model/joint.h>
+#include <map>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+namespace kinematic_calibration {
+
+/*
+ *
+ */
+class JointUpdate {
+
+public:
+	JointUpdate(urdf::Model model, string prefix);
+	virtual ~JointUpdate();
+
+	void setOffsets(map<string, double> offsets);
+
+	void getModifiedJoints(vector<urdf::Joint>& joints);
+
+	void writeCalibrationData(const string& filename);
+
+	void writeCalibrationData(const map<string, double> offsets,
+			const string& filename);
+
+private:
+	urdf::Model model;
+	const map<string, double> offsets;
+	string prefix;
+};
+
+} /* namespace kinematic_calibration */
+
+#endif /* JOINTUPDATE_H_ */
