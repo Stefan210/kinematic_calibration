cd20141df74e1a111664245676715896791e420f
Stefan Wrobel <wrobels@informatik.uni-freiburg.de>
2013-12-06 14:12:21 +0100

    Inherit from g2o::BaseVertex and implement the abstract methods.

--- src/kinematic_calibration/include/optimization/CameraIntrinsicsVertex.h
+++ src/kinematic_calibration/include/optimization/CameraIntrinsicsVertex.h
@@ -8,15 +8,36 @@
 #ifndef CAMERAINTRINSICSVERTEX_H_
 #define CAMERAINTRINSICSVERTEX_H_
 
+#include <boost/array.hpp>
+#include <g2o/core/base_vertex.h>
+#include <sensor_msgs/CameraInfo.h>
+
+using namespace g2o;
+
 namespace kinematic_calibration {
+
+typedef boost::array<double, 9> CameraIntrinsicsType;
 
 /*
  *
  */
-class CameraIntrinsicsVertex {
+class CameraIntrinsicsVertex: BaseVertex<4, CameraIntrinsicsType> {
 public:
-	CameraIntrinsicsVertex();
+	CameraIntrinsicsVertex(sensor_msgs::CameraInfo cameraInfo);
 	virtual ~CameraIntrinsicsVertex();
+
+	virtual bool read(std::istream&) {
+		return false;
+	}
+	virtual bool write(std::ostream&) const {
+		return false;
+	}
+	virtual void oplusImpl(const double*);
+	virtual void setToOriginImpl();
+
+protected:
+	sensor_msgs::CameraInfo cameraInfo;
+
 };
 
 } /* namespace kinematic_calibration */
--- src/kinematic_calibration/src/optimization/CameraIntrinsicsVertex.cpp
+++ src/kinematic_calibration/src/optimization/CameraIntrinsicsVertex.cpp
@@ -7,15 +7,31 @@
 
 #include "../../include/optimization/CameraIntrinsicsVertex.h"
 
+#define K_FX_IDX 0
+#define K_FY_IDX 4
+#define K_CX_IDX 2
+#define K_CY_IDX 5
+
 namespace kinematic_calibration {
 
-CameraIntrinsicsVertex::CameraIntrinsicsVertex() {
-	// TODO Auto-generated constructor stub
+CameraIntrinsicsVertex::CameraIntrinsicsVertex(
+		sensor_msgs::CameraInfo cameraInfo) :
+		cameraInfo(cameraInfo) {
 
 }
 
 CameraIntrinsicsVertex::~CameraIntrinsicsVertex() {
-	// TODO Auto-generated destructor stub
+}
+
+void CameraIntrinsicsVertex::oplusImpl(const double* delta) {
+	this->_estimate[K_FX_IDX] += delta[0];
+	this->_estimate[K_FY_IDX] += delta[1];
+	this->_estimate[K_CX_IDX] += delta[2];
+	this->_estimate[K_CY_IDX] += delta[3];
+}
+
+void CameraIntrinsicsVertex::setToOriginImpl() {
+	this->_estimate = cameraInfo.K;
 }
 
 } /* namespace kinematic_calibration */
