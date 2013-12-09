19b2b80b1c26b3bf8196e7ef05ce3f7104e69e34
Stefan Wrobel <wrobels@nougat.informatik.uni-freiburg.de>
2013-12-09 16:38:15 +0100

    Improve implementation (note: still not working)

--- src/kinematic_calibration/src/data_capturing/ImagePreprocessingNode.cpp
+++ src/kinematic_calibration/src/data_capturing/ImagePreprocessingNode.cpp
@@ -13,6 +13,8 @@
 #include <ros/init.h>
 #include <sensor_msgs/image_encodings.h>
 
+using namespace std;
+
 namespace kinematic_calibration {
 
 ImagePreprocessingNode::ImagePreprocessingNode() :
@@ -26,35 +28,48 @@ ImagePreprocessingNode::~ImagePreprocessingNode() {
 }
 
 void ImagePreprocessingNode::imageCb(const sensor_msgs::ImageConstPtr& msg) {
-	cv::Mat image;
 	cv_bridge::CvImagePtr input_bridge;
 	input_bridge = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::BGR8);
-	this->difference = cv::Mat(image);
-
+    std::cout << "Image received.\n";
 	// remove vignetting
 	if (!differenceInitialized) {
-		int x = input_bridge->image.cols / 2;
-		int y = input_bridge->image.rows / 2;
-		int value = input_bridge->image.at<int>(x, y);
-		int cols = input_bridge->image.cols, rows = input_bridge->image.rows;
+        this->difference = input_bridge->image.clone();
+        int x = (int) input_bridge->image.cols / 2;
+        int y = (int) input_bridge->image.rows / 2;
+        uchar value_b = input_bridge->image.at<uchar>(y, x, 0);
+        uchar value_g = input_bridge->image.at<uchar>(y, x, 1);
+        uchar value_r = input_bridge->image.at<uchar>(y, x, 2);
+        int cols = input_bridge->image.cols, rows = input_bridge->image.rows;
 		for (int i = 0; i < rows; i++) {
-			for (int j = 0; j < cols; j++) {
-				this->difference.at<int>(i, j) = input_bridge->image.at<int>(i,
-						j) - value;
-			}
-		}
-		differenceInitialized = true;
+            for (int j = 0; j < cols; j++) {
+                int difference_b = value_b - input_bridge->image.ptr<uchar>(i)[3*j+0];
+                int difference_g = value_g - input_bridge->image.ptr<uchar>(i)[3*j+1];
+                int difference_r = value_r - input_bridge->image.ptr<uchar>(i)[3*j+2];
+                difference_b > 0 ? difference_b : 0;
+                difference_g > 0 ? difference_g : 0;
+                difference_r > 0 ? difference_r : 0;
+                cout << difference_b << " " << difference_g << " " << difference_r << endl;
+                this->difference.ptr<uchar>(i)[3*j+0] = (unsigned char)difference_b;
+                this->difference.ptr<uchar>(i)[3*j+1] = (unsigned char)difference_g;
+                this->difference.ptr<uchar>(i)[3*j+2] = (unsigned char)difference_r;
+            }
+        }
+        differenceInitialized = true;std::cout << "difference initialized.\n";
+        cout << difference << endl;
 	}
-
+std::cout << "publishing image.\n";
 	// subtract the difference
-	input_bridge->image = input_bridge->image - difference;
+    input_bridge->image = input_bridge->image - difference;
 	pub.publish(input_bridge->toImageMsg());
 }
 
 } /* namespace kinematic_calibration */
 
+using namespace kinematic_calibration;
+
 int main(int argc, char** argv) {
 	ros::init(argc, argv, "imagePreprocessingNode");
+    ImagePreprocessingNode node;
 	ros::spin();
 	return 0;
 }
