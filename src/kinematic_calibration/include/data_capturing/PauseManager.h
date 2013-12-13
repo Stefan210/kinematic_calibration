1c5b2cc8ed51843220b4e3f5d75cadcac3589b66
Stefan Wrobel <wrobels@informatik.uni-freiburg.de>
2013-12-13 13:15:23 +0100

    Add class "PauseManager".

---
+++ src/kinematic_calibration/include/data_capturing/PauseManager.h
@@ -0,0 +1,56 @@
+/*
+ * PauseManager.h
+ *
+ *  Created on: 13.12.2013
+ *      Author: stefan
+ */
+
+#ifndef PAUSEMANAGER_H_
+#define PAUSEMANAGER_H_
+
+#include <kinematic_calibration/CmdPauseService.h>
+#include <ros/node_handle.h>
+#include <ros/service_server.h>
+#include <set>
+#include <string>
+
+using namespace ros;
+using namespace std;
+
+namespace kinematic_calibration {
+
+/**
+ * Class for determining whether it should be paused.
+ */
+class PauseManager {
+public:
+	/**
+	 * Constructor.
+	 */
+	PauseManager();
+
+	/**
+	 * Deconstructor.
+	 */
+	virtual ~PauseManager();
+
+	/**
+	 * If there is at least one request for a pause,
+	 * sleep and check periodically for new pause/resume requests.
+	 */
+	void pauseIfRequested();
+
+protected:
+	bool pauseCb(CmdPauseService::Request& req, CmdPauseService::Response& res);
+	bool resumeCb(CmdPauseService::Request& req, CmdPauseService::Response& res);
+
+private:
+	NodeHandle nh;
+	ServiceServer pauseService;
+	ServiceServer resumeService;
+	set<string> pauseReasons;
+};
+
+} /* namespace kinematic_calibration */
+
+#endif /* PAUSEMANAGER_H_ */
---
+++ src/kinematic_calibration/src/data_capturing/PauseManager.cpp
@@ -0,0 +1,46 @@
+/*
+ * PauseManager.cpp
+ *
+ *  Created on: 13.12.2013
+ *      Author: stefan
+ */
+
+#include "../../include/data_capturing/PauseManager.h"
+
+namespace kinematic_calibration {
+
+PauseManager::PauseManager() {
+	pauseService = nh.advertiseService(
+			string("/kinematic_calibration/data_capture/pause"),
+			&PauseManager::pauseCb, this);
+	resumeService = nh.advertiseService(
+			string("kinematic_calibration/data_capture/resume"),
+			&PauseManager::resumeCb, this);
+}
+
+PauseManager::~PauseManager() {
+	// TODO Auto-generated destructor stub
+}
+
+bool PauseManager::pauseCb(CmdPauseService::Request& req,
+		CmdPauseService::Response& res) {
+	this->pauseReasons.insert(req.reason);
+	ROS_INFO("Pause requested for reason %s.", req.reason);
+	return true;
+}
+
+bool PauseManager::resumeCb(CmdPauseService::Request& req,
+		CmdPauseService::Response& res) {
+	this->pauseReasons.erase(req.reason);
+	ROS_INFO("Resume requested for reason %s.", req.reason);
+	return true;
+}
+
+void PauseManager::pauseIfRequested() {
+	while(!this->pauseReasons.empty()) {
+		usleep(0.1 * 1e-6);
+		ros::spinOnce();
+	}
+}
+
+} /* namespace kinematic_calibration */
