f2f31da69fd6b14c67f5783ba92236ce5d1f00f8
Stefan Wrobel <wrobels@informatik.uni-freiburg.de>
2014-03-06 14:30:30 +0100

    Add stub for PoseSelectionNode.

---
+++ src/kinematic_calibration/include/common/PoseSelectionNode.h
@@ -0,0 +1,24 @@
+/*
+ * PoseSelectionNode.h
+ *
+ *  Created on: 06.03.2014
+ *      Author: stefan
+ */
+
+#ifndef POSESELECTIONNODE_H_
+#define POSESELECTIONNODE_H_
+
+namespace kinematic_calibration {
+
+/*
+ *
+ */
+class PoseSelectionNode {
+public:
+	PoseSelectionNode();
+	virtual ~PoseSelectionNode();
+};
+
+} /* namespace kinematic_calibration */
+
+#endif /* POSESELECTIONNODE_H_ */
---
+++ src/kinematic_calibration/src/common/PoseSelectionNode.cpp
@@ -0,0 +1,21 @@
+/*
+ * PoseSelectionNode.cpp
+ *
+ *  Created on: 06.03.2014
+ *      Author: stefan
+ */
+
+#include "../../include/common/PoseSelectionNode.h"
+
+namespace kinematic_calibration {
+
+PoseSelectionNode::PoseSelectionNode() {
+	// TODO Auto-generated constructor stub
+
+}
+
+PoseSelectionNode::~PoseSelectionNode() {
+	// TODO Auto-generated destructor stub
+}
+
+} /* namespace kinematic_calibration */
