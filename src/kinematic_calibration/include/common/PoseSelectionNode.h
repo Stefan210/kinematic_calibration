3ca231d335d352a3e5b0a48a7bca6a96b1c52705
Stefan Wrobel <wrobels@informatik.uni-freiburg.de>
2014-03-06 15:12:33 +0100

    Implement initialization of PoseSelectionNode via ROS.

--- src/kinematic_calibration/include/common/PoseSelectionNode.h
+++ src/kinematic_calibration/include/common/PoseSelectionNode.h
@@ -8,15 +8,91 @@
 #ifndef POSESELECTIONNODE_H_
 #define POSESELECTIONNODE_H_
 
+#include <boost/smart_ptr/shared_ptr.hpp>
+#include <image_geometry/pinhole_camera_model.h>
+#include <ros/node_handle.h>
+
+#include "../optimization/KinematicCalibrationState.h"
+#include "KinematicChain.h"
+#include "ModelLoader.h"
+
 namespace kinematic_calibration {
 
-/*
- *
+/**
+ * Node for the selection of poses for the calibration.
  */
 class PoseSelectionNode {
 public:
+	/**
+	 * Constructor.
+	 */
 	PoseSelectionNode();
+
+	/**
+	 * Desctructor.
+	 */
 	virtual ~PoseSelectionNode();
+
+	/**
+	 * Initializes from ROS.
+	 */
+	void initialize();
+
+protected:
+	/**
+	 * Initializes the kinematic chain for which the poses should be selected.
+	 */
+	virtual void initiializeKinematicChain();
+
+	/**
+	 * Initializes the initial state.
+	 */
+	virtual void initializeState();
+
+	/**
+	 * Initializes the camera model.
+	 */
+	virtual void initializeCamera();
+
+	/**
+	 * Pointer to the kinematic chain which is used.
+	 */
+	boost::shared_ptr<KinematicChain> kinematicChainPtr;
+
+	/**
+	 * Pointer to the initial/current state
+	 */
+	boost::shared_ptr<KinematicCalibrationState> initialState;
+
+	/**
+	 * Camera model.
+	 */
+	image_geometry::PinholeCameraModel cameraModel;
+
+	// TODO: source of poses / set of poses
+
+	// TODO: strategy for pose selection
+
+	// TODO: observability index strategy
+
+private:
+	void camerainfoCallback(
+			const sensor_msgs::CameraInfoConstPtr& msg);
+
+	/**
+	 * NodeHandle instance.
+	 */
+	ros::NodeHandle nh;
+
+	/**
+	 * Subscriber for camera info messages.
+	 */
+	ros::Subscriber cameraInfoSubscriber;
+
+	/**
+	 * Source for loading the robot model.
+	 */
+	ModelLoader modelLoader;
 };
 
 } /* namespace kinematic_calibration */
--- src/kinematic_calibration/src/common/PoseSelectionNode.cpp
+++ src/kinematic_calibration/src/common/PoseSelectionNode.cpp
@@ -7,15 +7,108 @@
 
 #include "../../include/common/PoseSelectionNode.h"
 
+#include <boost/smart_ptr/make_shared.hpp>
+//#include <kdl/kdl.hpp>
+#include <kdl/tree.hpp>
+#include <opencv2/core/core.hpp>
+#include <opencv2/core/operations.hpp>
+#include <ros/callback_queue.h>
+#include <ros/console.h>
+#include <ros/init.h>
+#include <ros/subscriber.h>
+#include <rosconsole/macros_generated.h>
+#include <sensor_msgs/CameraInfo.h>
+#include <tf/LinearMath/Quaternion.h>
+#include <tf/LinearMath/Transform.h>
+#include <tf/LinearMath/Vector3.h>
+#include <urdf/model.h>
+#include <urdf_model/joint.h>
+#include <urdf_model/pose.h>
+#include <iostream>
+#include <string>
+
 namespace kinematic_calibration {
 
-PoseSelectionNode::PoseSelectionNode() {
-	// TODO Auto-generated constructor stub
+using namespace std;
 
+PoseSelectionNode::PoseSelectionNode() {
+	initialize();
 }
 
 PoseSelectionNode::~PoseSelectionNode() {
-	// TODO Auto-generated destructor stub
+
+}
+
+void PoseSelectionNode::initialize() {
+	this->initializeCamera();
+	this->initiializeKinematicChain();
+	this->initializeState();
+}
+
+void PoseSelectionNode::initiializeKinematicChain() {
+	// instantiate the model loader
+	KDL::Tree kdlTree;
+	modelLoader.initializeFromRos();
+	modelLoader.getKdlTree(kdlTree);
+
+	// instantiate the kinematic chain
+	string chainName, chainRoot, chainTip;
+	nh.getParam("chain_name", chainName);
+	nh.getParam("chain_root", chainRoot);
+	nh.getParam("chain_tip", chainTip);
+
+	this->kinematicChainPtr = boost::make_shared<KinematicChain>(kdlTree,
+			chainName, chainRoot, chainTip);
+}
+
+void PoseSelectionNode::initializeState() {
+	// TODO: (optionally) do not use "zero initialization" for the joint offsets and marker
+	this->initialState = boost::make_shared<KinematicCalibrationState>();
+
+	// initialize transform from camera to head
+	string cameraJointName = "CameraBottom"; // todo: parameterize!
+	urdf::Model model;
+	this->modelLoader.getUrdfModel(model);
+	urdf::Joint cameraJoint = *model.getJoint(cameraJointName);
+	urdf::Pose headPitchToCameraPose =
+			cameraJoint.parent_to_joint_origin_transform;
+	tf::Transform headToCamera = tf::Transform(
+			tf::Quaternion(headPitchToCameraPose.rotation.x,
+					headPitchToCameraPose.rotation.y,
+					headPitchToCameraPose.rotation.z,
+					headPitchToCameraPose.rotation.w),
+			tf::Vector3(headPitchToCameraPose.position.x,
+					headPitchToCameraPose.position.y,
+					headPitchToCameraPose.position.z));
+	initialState->cameraToHeadTransformation = headToCamera;
+
+	// initialize the camera intrinsics
+	initialState->cameraInfo = cameraModel.cameraInfo();
+}
+
+void PoseSelectionNode::initializeCamera() {
+	// TODO: parameterize!
+	string topic = "/nao_camera/image_raw";
+	cameraInfoSubscriber = nh.subscribe(topic, 1,
+			&PoseSelectionNode::camerainfoCallback, this);
+	while (ros::getGlobalCallbackQueue()->isEmpty()) {
+		ROS_INFO("Waiting for camera info message...");
+		//ros::Duration(0.5).sleep();
+	}
+	ros::getGlobalCallbackQueue()->callAvailable();
+	cameraInfoSubscriber.shutdown();
+}
+
+void PoseSelectionNode::camerainfoCallback(
+		const sensor_msgs::CameraInfoConstPtr& msg) {
+	if (cameraModel.fromCameraInfo(msg)) {
+		ROS_INFO("Camera model set.");
+		cout << "Initial intrinsics: " << cameraModel.fullIntrinsicMatrix()
+				<< endl;
+	}
+
+	else
+		ROS_FATAL("Camera model could not be set!");
 }
 
 } /* namespace kinematic_calibration */
