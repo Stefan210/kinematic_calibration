9b28838fbcd81342786bc30185ae58bb277d0ffd
Stefan Wrobel <wrobels@informatik.uni-freiburg.de>
2014-03-06 13:47:17 +0100

    Add stubs for the four concrete observability indices.

--- src/kinematic_calibration/include/common/ObservabilityIndex.h
+++ src/kinematic_calibration/include/common/ObservabilityIndex.h
@@ -24,6 +24,56 @@ public:
 	 * Destructor.
 	 */
 	virtual ~ObservabilityIndex();
+
+	/**
+	 * Calculates the observability index based on the given pose set.
+	 * @param[in] poseSet The pose set for which the index should be calculated.
+	 * @param[out] index The calculated index.
+	 */
+	void calculateIndex(const PoseSet& poseSet, double& index) = 0;
+};
+
+class ProductSingularValuesIndex: public ObservabilityIndex {
+public:
+
+	/**
+	 * Index := root(s1*...*sL)/sqrt(m)
+	 * s1, ..., sL = singular values
+	 * m = number of poses
+	 */
+	void calculateIndex(const PoseSet& poseSet, double& index);
+};
+
+class InverseConditionNumberIndex: public ObservabilityIndex {
+public:
+
+	/**
+	 * Index := sL / s1 = 1/cond(J)
+	 * @param poseSet
+	 * @param index
+	 */
+	void calculateIndex(const PoseSet& poseSet, double& index);
+};
+
+class MinimumSingularValueIndex: public ObservabilityIndex {
+public:
+
+	/**
+	 * Index := sL
+	 * sL = minimum singular value
+	 */
+	void calculateIndex(const PoseSet& poseSet, double& index);
+};
+
+class NoiseAmplificationIndex: public ObservabilityIndex {
+public:
+
+	/**
+	 * Index := sL * sL / s1
+	 * sL = minimum singular value
+	 * s1 = maximum singular value
+	 */
+	void calculateIndex(const PoseSet& poseSet, double& index);
 };
 
 } /* namespace kinematic_calibration */
