108f4f56afa461462284a6fb74c7f9fae87306c9
Stefan Wrobel <wrobels@informatik.uni-freiburg.de>
2014-03-26 17:12:27 +0100

    Add shebang.

--- src/kinematic_calibration/scripts/JointStatesToPose.py
+++ src/kinematic_calibration/scripts/JointStatesToPose.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 import sys
 import yaml
 from itertools import repeat
--- src/kinematic_calibration/scripts/MeasurementPlot.py
+++ src/kinematic_calibration/scripts/MeasurementPlot.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 import rospy
 from kinematic_calibration.msg import measurementData
 import numpy as np
