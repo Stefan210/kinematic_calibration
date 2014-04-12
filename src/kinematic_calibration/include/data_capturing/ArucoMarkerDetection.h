ce45486c7c42e40aaf4ae3857a0e0f6ff47c00bc
Stefan Wrobel <wrobels@informatik.uni-freiburg.de>
2014-04-12 16:18:18 +0200

    Implement ArUco marker detection adapter class, (simple) unit test and a sample marker.

---
+++ src/kinematic_calibration/include/data_capturing/ArucoMarkerDetection.h
@@ -0,0 +1,41 @@
+/*
+ * ArucoMarkerDetection.h
+ *
+ *  Created on: 11.04.2014
+ *      Author: stefan
+ */
+
+#ifndef ARUCOMARKERDETECTION_H_
+#define ARUCOMARKERDETECTION_H_
+
+#include <opencv2/core/core.hpp>
+#include <sensor_msgs/Image.h>
+#include <vector>
+
+#include "../../lib/aruco/src/marker.h"
+#include "SinglePointMarkerDetection.h"
+
+namespace kinematic_calibration {
+
+using namespace std;
+
+/**
+ * Class for detecting markers using the ArUco library.
+ * Further informations: http://www.uco.es/investiga/grupos/ava/node/26
+ */
+class ArucoMarkerDetection : public SinglePointMarkerDetection {
+public:
+	ArucoMarkerDetection();
+	virtual ~ArucoMarkerDetection();
+
+	bool detect(const sensor_msgs::ImageConstPtr& in_msg, vector<double>& out);
+	bool detect(const cv::Mat& inImage, vector<double>& out);
+	void drawMarker(cv::Mat& image);
+
+private:
+	aruco::Marker currentMarker;
+};
+
+} /* namespace kinematic_calibration */
+
+#endif /* ARUCOMARKERDETECTION_H_ */
---
+++ src/kinematic_calibration/src/data_capturing/ArucoMarkerDetection.cpp
@@ -0,0 +1,88 @@
+/*
+ * ArucoMarkerDetection.cpp
+ *
+ *  Created on: 11.04.2014
+ *      Author: stefan
+ */
+
+#include "../../include/data_capturing/ArucoMarkerDetection.h"
+
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <sensor_msgs/Image.h>
+#include <iostream>
+#include <vector>
+
+#include "../../lib/aruco/src/marker.h"
+#include "../../lib/aruco/src/markerdetector.h"
+
+namespace kinematic_calibration {
+
+using namespace std;
+using namespace cv;
+using namespace aruco;
+
+ArucoMarkerDetection::ArucoMarkerDetection() {
+	// nothing to do
+}
+
+ArucoMarkerDetection::~ArucoMarkerDetection() {
+	// nothing to do
+}
+
+bool ArucoMarkerDetection::detect(const sensor_msgs::ImageConstPtr& in_msg,
+		vector<double>& out) {
+	// get the input image
+	cv::Mat inImage;
+	this->msgToImg(in_msg, inImage);
+	return detect(inImage, out);
+}
+
+bool ArucoMarkerDetection::detect(const cv::Mat& inImage, vector<double>& out) {
+	MarkerDetector detector;
+	vector<Marker> markers;
+	// try to detect
+	detector.detect(inImage, markers);
+	if(markers.size() < 1) {
+		// no marker found
+		return false;
+	}
+
+	cv::Mat outImage = inImage.clone();
+	saveImage(outImage);
+
+	double x, y;
+	cv::Point2f centerPoint(0.0, 0.0);
+	for(int i = 0; i < 4; i++) {
+		centerPoint += markers[0][i];
+	}
+	centerPoint.x = centerPoint.x / 4;
+	centerPoint.y = centerPoint.y / 4;
+
+	out.push_back(centerPoint.x);
+	out.push_back(centerPoint.y);
+	savePosition(out);
+	this->currentMarker = markers[0];
+
+	//for each marker, draw info and its boundaries in the image
+	/*
+	for (unsigned int i = 0; i < markers.size(); i++) {
+		cout << markers[i] << endl;
+		markers[i].draw(outImage, Scalar(0, 0, 255), 2);
+	}
+	cv::imshow("aruco", outImage);
+	cv::waitKey(0); //wait for key to be pressed
+	*/
+
+	return true;
+}
+
+void ArucoMarkerDetection::drawMarker(cv::Mat& image) {
+	// draw center
+	SinglePointMarkerDetection::drawMarker(image);
+
+	// draw contour
+	this->currentMarker.draw(image, Scalar(0, 0, 255), 2);
+}
+
+} /* namespace kinematic_calibration */
