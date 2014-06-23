15e9ee30b2fa76f52fcf4d7c51175e2b1b5a02b1
Stefan Wrobel <wrobels@informatik.uni-freiburg.de>
2014-06-23 11:21:13 +0200

    Pose marker: use the center of the marker as origin of the coordinate system.

--- src/kinematic_calibration/src/optimization/CheckerboardMeasurementEdge.cpp
+++ src/kinematic_calibration/src/optimization/CheckerboardMeasurementEdge.cpp
@@ -157,29 +157,29 @@ void ArucoPoseMeasurementEdge::setError(tf::Transform cameraToMarker) {
 	// point on the x axis of the marker's coordinate system
 	double xAxisX, yAxisX;
 	tf::Transform alongXAxis(tf::Quaternion(0, 0, 0, 1),
-			tf::Vector3(squareLength, 0, 0));
+			tf::Vector3(squareLength / 2, - squareLength / 2, 0));
 	this->frameImageConverter->project((alongXAxis * cameraToMarker).inverse(),
 			xAxisX, yAxisX);
 
 	// point on the y axis of the marker's coordinate system
 	double xAxisY, yAxisY;
 	tf::Transform alongYAxis(tf::Quaternion(0, 0, 0, 1),
-			tf::Vector3(0, squareLength, 0));
+			tf::Vector3(squareLength / 2, squareLength / 2, 0));
 	this->frameImageConverter->project((alongYAxis * cameraToMarker).inverse(),
 			xAxisY, yAxisY);
 
 	// set error
 	double offset = 2;
 
-	// top left corner
-	this->_error[0] = measurement.marker_data[offset + 0 + 0] - xOrigin;
-	this->_error[1] = measurement.marker_data[offset + 0 + 1] - yOrigin;
+	// center (r)
+	this->_error[0] = measurement.marker_data[0] - xOrigin;
+	this->_error[1] = measurement.marker_data[1] - yOrigin;
 
-	// one right from the top left corner
-	this->_error[2] = measurement.marker_data[offset + 2 + 0] - xAxisX;
-	this->_error[3] = measurement.marker_data[offset + 2 + 1] - yAxisX;
+	// top right (g)
+	this->_error[2] = measurement.marker_data[offset + 0 + 0] - xAxisX;
+	this->_error[3] = measurement.marker_data[offset + 0 + 1] - yAxisX;
 
-	// one down from the top left corner
+	// bottom right (b)
 	this->_error[4] = measurement.marker_data[offset + 6 + 0] - xAxisY;
 	this->_error[5] = measurement.marker_data[offset + 6 + 1] - yAxisY;
 
@@ -195,16 +195,16 @@ void ArucoPoseMeasurementEdge::setError(tf::Transform cameraToMarker) {
 		cv::line(outImg, cv::Point(xOrigin - 3, yOrigin + 3),
 				cv::Point(xOrigin + 3, yOrigin - 3), CV_RGB(255, 0, 0));
 		cv::circle(outImg,
-				cv::Point(measurement.marker_data[offset],
-						measurement.marker_data[offset + 1]), 3,
+				cv::Point(measurement.marker_data[0],
+						measurement.marker_data[1]), 3,
 				CV_RGB(255, 0, 0));
 		cv::line(outImg, cv::Point(xAxisX - 3, yAxisX - 3),
 				cv::Point(xAxisX + 3, yAxisX + 3), CV_RGB(0, 255, 0));
 		cv::line(outImg, cv::Point(xAxisX - 3, yAxisX + 3),
 				cv::Point(xAxisX + 3, yAxisX - 3), CV_RGB(0, 255, 0));
 		cv::circle(outImg,
-				cv::Point(measurement.marker_data[offset + 2],
-						measurement.marker_data[offset + 2 + 1]), 3,
+				cv::Point(measurement.marker_data[offset + 0],
+						measurement.marker_data[offset + 0 + 1]), 3,
 				CV_RGB(0, 255, 0));
 		cv::line(outImg, cv::Point(xAxisY - 3, yAxisY - 3),
 				cv::Point(xAxisY + 3, yAxisY + 3), CV_RGB(0, 0, 255));
